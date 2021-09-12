4ae159f666788cf410e79c44dd3e9e8e3bde44b0
PLAWSO <64998692+PLAWSO@users.noreply.github.com>
2021-09-12 18:25:46 -0500

    changed name of form main, the add form option wasn't showing up so I couldn't finish setup atm

--- CSC470-P2/CSC470-P2/FormMain.cs
+++ CSC470-P2/CSC470-P2/FormMain.cs
@@ -10,11 +10,12 @@ using System.Windows.Forms;
 
 namespace CSC470_P2
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
+        
         }
     }
 }
